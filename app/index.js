ba3ffdaf18a8e98ee258bfca32042099577cc324
Aaron Davis <aaronrdavis@gmail.com>
2015-05-13 15:36:55 -0500

    changed property name

--- app/index.js
+++ app/index.js
@@ -218,10 +218,10 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copyTpl(
         this.templatePath('api/Properties/_assemblyinfo.cs'),
         this.destinationPath(this.propertiesDirectory + 'AssemblyInfo.cs'),
-        {
-          applicationName: this.props.applicationName,
-          apiAssemblyGuid: this.props.apiAssemblyGuid
-        }
+        [
+          { applicationName: this.props.applicationName },
+          { apiAssemblyGuid: this.props.apiAssemblyGuid }
+        ]
         );
     },
     rootFiles: function () {
