41249115629850b82b8671c7e578733c38d870c2
Aaron Davis <aaronrdavis@gmail.com>
2015-05-13 21:15:09 -0500

    test changes for app run

--- test/test-app.js
+++ test/test-app.js
@@ -6,47 +6,7 @@ var helpers = require('yeoman-generator').test;
 var temp = require('temp').track();
 
 describe('generator-webapi-owin-jwt-aspnet-identity', function () {
-  var expectedProjectFiles = [
-    'MyApplication.sln',
-    'packages/repositories.conig',
-    'MyApplication.API/AppStart/WebApiConfig.cs',
-    'MyApplication.API/Authentication/EmailTemplates/ConfirmEmailAddressEmail.html',
-    'MyApplication.API/Authentication/EmailTemplates/ResetPasswordEmail.html',
-    'MyApplication.API/Authentication/ApplicationJWTFormat.cs',
-    'MyApplication.API/Authentication/ApplicationUser.cs',
-    'MyApplication.API/Authentication/AuthenticationContext.cs',
-    'MyApplication.API/Authentication/AuthenticationEmailService.cs',
-    'MyApplication.API/Authentication/AuthenticationUserManager.cs',
-    'MyApplication.API/Authentication/OAuthProvider.cs',
-    'MyApplication.API/AutoMapping/AutoMappingExtensions.cs',
-    'MyApplication.API/AutoMapping/NewUserProfile.cs',
-    'MyApplication.API/AutoMapping/UserProfile.cs',
-    'MyApplication.API/Controllers/BaseController.cs',
-    'MyApplication.API/Controllers/UsersController.cs',
-    'MyApplication.API/Exceptions/APIException.cs',
-    'MyApplication.API/Exceptions/APIGlobalExceptionHandler.cs',
-    'MyApplication.API/Exceptions/APIResponseExceptionHandler.cs',
-    'MyApplication.API/Extensions/ExceptionExtensions.cs',
-    'MyApplication.API/Filters/APIExceptionFilterAttribute.cs',
-    'MyApplication.API/Filters/ModelValidationFilterAttribute.cs',
-    'MyApplication.API/Migrations/Configuration.cs',
-    'MyApplication.API/Models/Request/ConfirmEmailAddressRequest.cs',
-    'MyApplication.API/Models/Request/CreateUserRequest.cs',
-    'MyApplication.API/Models/Request/ResetPasswordRequest.cs',
-    'MyApplication.API/Models/Request/SendPasswordResetRequest.cs',
-    'MyApplication.API/Models/Request/UserRequest.cs',
-    'MyApplication.API/Models/Response/DefaultResponse.cs',
-    'MyApplication.API/Models/Response/UserResponse.cs',
-    'MyApplication.API/Models/NewUserModel.cs',
-    'MyApplication.API/Models/UserModel.cs',
-    'MyApplication.API/Properties/AssemblyInfo.cs',
-    'MyApplication.API.csproj',
-    'MyApplication.API/packages.config',
-    'MyApplication.API/Startup.cs',
-    'MyApplication.API/Web.config',
-    'MyApplication.API/Web.Debug.config',
-    'MyApplication.API/Web.Release.config'
-  ];
+
 
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
@@ -57,6 +17,7 @@ describe('generator-webapi-owin-jwt-aspnet-identity', function () {
       this.app = helpers.createGenerator('webapi-owin-jwt-aspnet-identity:app', [
         '../../app'
       ]);
+      this.app.options['skip-install'] = true;
 
       helpers.mockPrompt(this.app, {
         'applicationName': 'MyApplication'
@@ -64,7 +25,6 @@ describe('generator-webapi-owin-jwt-aspnet-identity', function () {
       helpers.mockPrompt(this.app, {
         'dbServerName': '.\sqlexpress'
       });
-
       done();
     }.bind(this));
   });
@@ -74,7 +34,48 @@ describe('generator-webapi-owin-jwt-aspnet-identity', function () {
   });
 
   it('should run and create all of the application files', function (done) {
-    this.app.run({}, function () {
+    var expectedProjectFiles = [
+      'MyApplication.sln',
+      'packages/repositories.conig',
+      'MyApplication.API/AppStart/WebApiConfig.cs',
+      'MyApplication.API/Authentication/EmailTemplates/ConfirmEmailAddressEmail.html',
+      'MyApplication.API/Authentication/EmailTemplates/ResetPasswordEmail.html',
+      'MyApplication.API/Authentication/ApplicationJWTFormat.cs',
+      'MyApplication.API/Authentication/ApplicationUser.cs',
+      'MyApplication.API/Authentication/AuthenticationContext.cs',
+      'MyApplication.API/Authentication/AuthenticationEmailService.cs',
+      'MyApplication.API/Authentication/AuthenticationUserManager.cs',
+      'MyApplication.API/Authentication/OAuthProvider.cs',
+      'MyApplication.API/AutoMapping/AutoMappingExtensions.cs',
+      'MyApplication.API/AutoMapping/NewUserProfile.cs',
+      'MyApplication.API/AutoMapping/UserProfile.cs',
+      'MyApplication.API/Controllers/BaseController.cs',
+      'MyApplication.API/Controllers/UsersController.cs',
+      'MyApplication.API/Exceptions/APIException.cs',
+      'MyApplication.API/Exceptions/APIGlobalExceptionHandler.cs',
+      'MyApplication.API/Exceptions/APIResponseExceptionHandler.cs',
+      'MyApplication.API/Extensions/ExceptionExtensions.cs',
+      'MyApplication.API/Filters/APIExceptionFilterAttribute.cs',
+      'MyApplication.API/Filters/ModelValidationFilterAttribute.cs',
+      'MyApplication.API/Migrations/Configuration.cs',
+      'MyApplication.API/Models/Request/ConfirmEmailAddressRequest.cs',
+      'MyApplication.API/Models/Request/CreateUserRequest.cs',
+      'MyApplication.API/Models/Request/ResetPasswordRequest.cs',
+      'MyApplication.API/Models/Request/SendPasswordResetRequest.cs',
+      'MyApplication.API/Models/Request/UserRequest.cs',
+      'MyApplication.API/Models/Response/DefaultResponse.cs',
+      'MyApplication.API/Models/Response/UserResponse.cs',
+      'MyApplication.API/Models/NewUserModel.cs',
+      'MyApplication.API/Models/UserModel.cs',
+      'MyApplication.API/Properties/AssemblyInfo.cs',
+      'MyApplication.API.csproj',
+      'MyApplication.API/packages.config',
+      'MyApplication.API/Startup.cs',
+      'MyApplication.API/Web.config',
+      'MyApplication.API/Web.Debug.config',
+      'MyApplication.API/Web.Release.config'
+    ];
+    this.app.run(function () {
       assert.file(expectedProjectFiles);
       done();
     });
